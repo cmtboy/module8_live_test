6b7dd95761220551b48fd6833f0070e98ff2515c
CmtBoy <alaminislam.dev@gmail.com>
2023-09-29 23:35:18 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Image Feed'),
+        ),
+        body: const ImageFeed(),
+      ),
+    );
+  }
+}
+
+class ImageFeed extends StatefulWidget {
+  const ImageFeed({super.key});
+
+  @override
+  ImageFeedState createState() => ImageFeedState();
+}
+
+class ImageFeedState extends State<ImageFeed> {
+  @override
+  Widget build(BuildContext context) {
+    final orientation = MediaQuery.of(context).orientation;
+
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: orientation == Orientation.portrait ? 1 : 2,
+      ),
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return Card(
+          child: Image.network('https://via.placeholder.com/150'),
+        );
+      },
+    );
+  }
+}
